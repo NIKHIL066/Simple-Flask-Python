e395f1f031c045b1fd8016d6c6d71e37c547f29a
NIKHIL066 <hegdecse066@yahoo.com>
2018-11-15 20:43:42 +0530

    Usage of Flask decorator

--- hello.py
+++ hello.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app=Flask(__name__)
+template_path='C:\\Python36\\Flask Projects\\template'
+app=Flask(__name__,template_folder=template_path)
 
-@app.route('/')
-
+@app.route('/index')
 def index():
-    return 'INDEX'
-
+    try:
+        return render_template('home.html')
+    except Exception as e:
+        print('Error: {0}'.format(e))
+        exit()
+        
+@app.route('/home')
+def home():
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        print('Error: {0}'.format(e))
+        exit()    
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
